2ef59f43845efb6015a372ee702e7f76372586b0
alejandroavinaa <127714455+alejandroavinaa@users.noreply.github.com>
2025-11-14 08:47:56 -0800

    Initial Commit

---
+++ Homework13_Alejandro_Avina/Homework13_Alejandro_AvinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework13_Alejandro_AvinaApp.swift
+//  Homework13_Alejandro_Avina
+//
+//  Created by Alejandro Avina on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Homework13_Alejandro_AvinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
